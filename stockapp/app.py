2530ddb0f3d4ff81fcb5b407c216048172e49b66
cullinank2 <cullinank2@mail.sacredheart.edu>
2026-03-13 13:18:57 -0400

    kjc debug kg_vis

--- stockapp/app.py
+++ stockapp/app.py
@@ -61,7 +61,7 @@ from visualizations import (
     plot_crowding_score
 )
 from chatbot import create_chatbot, SAMPLE_QUESTIONS
-from kg_visualizer import render_kg_tab
+# from kg_visualizer import render_kg_tab
 from period_analysis import (
     create_loading_comparison_chart,
     compute_procrustes_table,
