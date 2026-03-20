9b612e3dc93a20c0a6b59e5b032023e1683781b4
cullinank2 <cullinank2@mail.sacredheart.edu>
2026-03-20 17:17:51 -0400

    kjc edit alter render size of header

--- stockapp/app.py
+++ stockapp/app.py
@@ -138,7 +138,7 @@ st.markdown("""
 
     /* Main header styling */
     .main-header {
-        font-size: 3rem;
+        font-size: 1.5rem;
         color: #1f77b4;
         text-align: center;
         padding: 1rem 0;
