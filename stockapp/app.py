430aa97225e7099da7e9736ff3539f43e3cfb3ac
cullinank2 <cullinank2@mail.sacredheart.edu>
2026-03-20 16:20:38 -0400

    kjc edit render tab 1 custom header

--- stockapp/app.py
+++ stockapp/app.py
@@ -288,7 +288,7 @@ html[data-theme="dark"] {
 --------------------------------*/
 button[data-baseweb="tab"] * {
     font-size: 1.2rem !important;
-    font-weight: 800 !important;
+    font-weight: 900 !important;
 }
 
 /* -------------------------------
