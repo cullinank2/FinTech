3ba9da0bfadb2c1fc293fb4e990c603cce26c3d9
cullinank2 <cullinank2@mail.sacredheart.edu>
2026-03-20 16:08:05 -0400

    kjc edit render tab 1 custom header

--- stockapp/app.py
+++ stockapp/app.py
@@ -292,7 +292,7 @@ button[aria-selected="true"] * {
 --------------------------------*/
 div[role="tablist"] > button:nth-of-type(1) p {
     font-size: 1.4rem !important;
-    color: #ff4b4b !important;
+    color: #06B6D4 !important;
     font-weight: 500 !important;
 }
 
